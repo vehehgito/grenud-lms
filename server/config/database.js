67df914e94a89bd5be017b9722e3e95157b911b1
Neelanjan Mukherji <neelanjanmukherji@gmail.com>
2024-06-06 03:35:42 +0530

    Nodejs Connected with AWS RDS PostgreSQL

---
+++ server/config/database.js
@@ -0,0 +1,26 @@
+import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config({ path: "./config/config.env" });
+
+// const {DATABASE_URI} = process.env;
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
+const { Pool } = pg;
+
+const pool = new Pool({
+    user: PGUSER,
+    host: PGHOST,
+    database: PGDATABASE,
+    password: PGPASSWORD,
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+
+
+});
+
+
+
+
+export default pool;
